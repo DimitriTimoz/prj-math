3e4de4863993079167e0b0b6bed59a895aab7a91
DimitriTimoz <dimitri.timoz@orange.fr>
2022-12-04 13:11:31 +0100

    add init newton

--- main.pas
+++ main.pas
@@ -4,5 +4,5 @@ uses utils, dichotomieMethode, newtonMethode;
 
 begin
     dichotomie(0.0, 50.0, 0.00001).afficheResultat();
-    newton(0.00001).afficheResultat();
+    newton(1.0, 0.00001).afficheResultat();
 end.
--- newtonMethode.pas
+++ newtonMethode.pas
@@ -4,29 +4,26 @@ interface
 
 uses functions, utils;
 
-function newton(e: Double): TResultatMethode;
+function newton(a, e: Double): TResultatMethode;
 
 
 implementation
-function newton(e: Double): TResultatMethode;
+function newton(a, e: Double): TResultatMethode;
 var
-  a: Double;
   i: integer;
-
 begin
-    a := 1;
-    i := 0;
-    while abs(trajectoire(a)) > e do
-    begin
-        i := i + 1;
-        
-        a := a - trajectoire(a) / dtrajectoire(a);
-        
-        // Protection contre les boucles infinies
-        if i > max_iter then
-          break;
-    end;
-    newton.init(a, i, Newt);
+  i := 0;
+  while abs(trajectoire(a)) > e do
+  begin
+      i := i + 1;
+      
+      a := a - trajectoire(a) / dtrajectoire(a);
+      
+      // Protection contre les boucles infinies
+      if i > max_iter then
+        break;
+  end;
+  newton.init(a, i, Newt);
 end;
 
     
